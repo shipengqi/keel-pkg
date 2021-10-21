3e43860ccce3a30f498ace5d9459411fbc734366
shipengqi <pooky.shipengqi@gmail.com>
2021-10-21 20:38:56 +0800

    pack error

--- .github/scripts/post-pack.sh
+++ .github/scripts/post-pack.sh
@@ -5,28 +5,28 @@ PACK_HOME=${HOME}/pack
 cd ${PACK_HOME}
 ls -lh ${PACK_HOME}
 
+echo "Coping kubectl kubelet"
 sudo chmod 755 kubectl kubelet
 mkdir -p ${PACK_HOME}/src/kubernetes/bin/
 mv kubectl ${PACK_HOME}/src/kubernetes/bin/
 mv kubelet ${PACK_HOME}/src/kubernetes/bin/
-ls -lh ${PACK_HOME}/src/runtime/containerd/bin/
+ls -lh ${PACK_HOME}/src/runtime/kubernetes/bin/
 
+echo "Coping runtime"
 mkdir -p ${PACK_HOME}/src/runtime/containerd/bin/
-tar -xf containerd-*.tar.gz -C ${PACK_HOME}/src/runtime/containerd/
-tar -xf crictl-*.tar.gz -C ${PACK_HOME}/src/runtime/containerd/bin/
-rm -rf containerd-*.tar.gz
-rm -rf crictl-*.tar.gz
+tar -xf containerd-*${PACK_ARCH}.tar.gz -C ${PACK_HOME}/src/runtime/containerd/
+tar -xf crictl-*${PACK_ARCH}.tar.gz -C ${PACK_HOME}/src/runtime/containerd/bin/
+rm -rf containerd-*${PACK_ARCH}.tar.gz
+rm -rf crictl-*${PACK_ARCH}.tar.gz
 sudo chmod 755 runc.${PACK_ARCH}
-mv runc.${PACK_ARCH} ${PACK_HOME}/src/runtime/containerd/bin/
+mv runc.${PACK_ARCH} ${PACK_HOME}/src/runtime/containerd/bin/runc
 ls -lh ${PACK_HOME}/src/runtime/containerd/bin/
 
+echo "Coping images"
 mkdir -p ${PACK_HOME}/src/images
 cp ./images/* ${PACK_HOME}/src/images
 rm -rf ./images
 ls -lh ${PACK_HOME}/src/images
-
-echo ${KUBERNETES_VERSION} ${PACK_ARCH}
-cd ${PACK_HOME}/src
 
 TAR_NAME=kube-${KUBERNETES_VERSION}-${PACK_ARCH}.tar.gz
 
@@ -35,10 +35,12 @@ if [ -n "${BETA_VERSION}" ];then
 fi
 
 echo "Packing ${TAR_NAME}"
+cd ${PACK_HOME}/src
 tar -czvf ${PACK_HOME}/${TAR_NAME} .
 cd ${PACK_HOME}
 rm -rf ./src
 echo "Pack ${TAR_NAME} done!"
 
-./packer push -k ${PACK_HOME} -s ${PACK_HOME} --pkg-uri ${PACK_HOME}/${TAR_NAME}
-
+echo "Pushing ${TAR_NAME} ..."
+./packer push -k ${QINIU_ACCESS_KEY} -s ${QINIU_SECRET_KEY} --pkg-uri ${PACK_HOME}/${TAR_NAME}
+echo "Push ${TAR_NAME} done!"

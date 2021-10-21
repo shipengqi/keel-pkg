f04ade58083c2053314d24266194ccce22eefc4c
shipengqi <pooky.shipengqi@gmail.com>
2021-10-21 16:25:06 +0800

    fix workflow issues

--- .github/scripts/pre-sync.sh
+++ .github/scripts/pre-sync.sh
@@ -6,10 +6,9 @@ mkdir -p ${SYNC_HOME}
 cp synctl ${SYNC_HOME}
 cp image_set.json ${SYNC_HOME}
 
-cd ${SYNC_HOME}
-ls -lh
-
 docker run --rm -tid --name syncdb shipengqi/google_containers_sync_db top
 docker cp syncdb:/sync.bolt.db ${SYNC_HOME}
-
 docker kill syncdb
+
+cd ${SYNC_HOME}
+ls -lh
